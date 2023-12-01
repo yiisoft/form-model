0c5194da08a7b3c7357b714891784f96bd2ca438
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-12-01 22:04:51 +0300

    two

--- tests/ValidationRulesEnricherTest.php
+++ tests/ValidationRulesEnricherTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Yiisoft\Form\ThemeContainer;
 use Yiisoft\FormModel\Field;
+use Yiisoft\FormModel\Tests\Support\Form\TextForm;
 use Yiisoft\FormModel\Tests\Support\Form\UrlForm;
 use Yiisoft\FormModel\ValidationRulesEnricher;
 use Yiisoft\Test\Support\Container\SimpleContainer;
@@ -22,7 +23,7 @@ final class ValidationRulesEnricherTest extends TestCase
         );
     }
 
-    public static function dataEnrichFromValidationRules(): array
+    public static function dataUrl(): array
     {
         return [
             'required' => [
@@ -64,10 +65,49 @@ final class ValidationRulesEnricherTest extends TestCase
         ];
     }
 
-    #[DataProvider('dataEnrichFromValidationRules')]
-    public function testEnrichFromValidationRules(string $expected, string $property): void
+    #[DataProvider('dataUrl')]
+    public function testUrl(string $expected, string $property): void
     {
         $field = Field::url(new UrlForm(), $property)
+            ->hideLabel()
+            ->enrichFromValidationRules(true)
+            ->useContainer(false);
+
+        $this->assertSame($expected, $field->render());
+    }
+
+    public static function dataText(): array
+    {
+        return [
+            'required' => [
+                '<input type="text" id="textform-company" name="TextForm[company]" value required>',
+                'company',
+            ],
+            'has-length' => [
+                '<input type="text" id="textform-shortdesc" name="TextForm[shortdesc]" value maxlength="199" minlength="10">',
+                'shortdesc',
+            ],
+            'regex' => [
+                '<input type="text" id="textform-code" name="TextForm[code]" value pattern="\w+">',
+                'code',
+            ],
+            'regex-not' => [
+                '<input type="text" id="textform-nocode" name="TextForm[nocode]" value>',
+                'nocode',
+            ],
+            'required-with-when' => [
+                '<input type="text" id="textform-requiredwhen" name="TextForm[requiredWhen]">',
+                'requiredWhen',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataText
+     */
+    public function testText(string $expected, string $property): void
+    {
+        $field = Field::text(new TextForm(), $property)
             ->hideLabel()
             ->enrichFromValidationRules(true)
             ->useContainer(false);
