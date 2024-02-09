66fe52add7ccdd87bfd7da093e1856d9ff5c00b5
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-02-09 11:08:19 +0300

    Improve `FromHydrator` configuration: convert empty strings to null

--- config/di.php
+++ config/di.php
@@ -2,14 +2,33 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Definitions\Reference;
+use Yiisoft\Definitions\DynamicReference;
 use Yiisoft\FormModel\FormHydrator;
+use Yiisoft\Hydrator\Hydrator;
+use Yiisoft\Hydrator\TypeCaster\CompositeTypeCaster;
+use Yiisoft\Hydrator\TypeCaster\HydratorTypeCaster;
+use Yiisoft\Hydrator\TypeCaster\NullTypeCaster;
+use Yiisoft\Hydrator\TypeCaster\PhpNativeTypeCaster;
 use Yiisoft\Hydrator\Validator\ValidatingHydrator;
 
 return [
     FormHydrator::class => [
         '__construct()' => [
-            'hydrator' => Reference::to(ValidatingHydrator::class),
+            'hydrator' => DynamicReference::to([
+                'class' => ValidatingHydrator::class,
+                '__construct()' => [
+                    'hydrator' => DynamicReference::to([
+                        'class' => Hydrator::class,
+                        '__construct()' => [
+                            'typeCaster' => new CompositeTypeCaster(
+                                new NullTypeCaster(emptyString: true),
+                                new PhpNativeTypeCaster(),
+                                new HydratorTypeCaster(),
+                            ),
+                        ],
+                    ])
+                ],
+            ])
         ],
     ],
 ];
