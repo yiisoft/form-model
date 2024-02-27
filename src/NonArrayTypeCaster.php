71509c799d228ba203d1a174b6f5a6dd503d3273
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-02-27 15:39:16 +0300

    Add `NonArrayTypeCaster` (#28)

---
+++ src/NonArrayTypeCaster.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\FormModel;
+
+use ReflectionNamedType;
+use ReflectionType;
+use Yiisoft\Hydrator\Result;
+use Yiisoft\Hydrator\TypeCaster\TypeCastContext;
+use Yiisoft\Hydrator\TypeCaster\TypeCasterInterface;
+
+final class NonArrayTypeCaster implements TypeCasterInterface
+{
+    public function cast(mixed $value, TypeCastContext $context): Result
+    {
+        if (is_array($value)) {
+            return Result::fail();
+        }
+
+        if ($this->isArray($context->getReflectionType())) {
+            return Result::success([]);
+        }
+
+        return Result::fail();
+    }
+
+    private function isArray(?ReflectionType $type): bool
+    {
+        return $type instanceof ReflectionNamedType && $type->isBuiltin() && $type->getName() === 'array';
+    }
+}
