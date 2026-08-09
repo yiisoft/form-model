fb1d986a3abe9359cc1cce30317d0a374f1c26f1
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-09 15:02:24 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->disableComposerAutoloadPathScan()
@@ -11,4 +12,7 @@ return (new Configuration())
     ->addPathToScan(__DIR__ . '/src', isDev: false)
     ->addPathToScan(__DIR__ . '/tests', isDev: true)
     // Non-namespaced test fixture, manually `require`d (does not fit PSR-4 autoloading).
-    ->ignoreUnknownClasses(['NonNamespacedForm']);
+    ->ignoreUnknownClasses(['NonNamespacedForm'])
+    // `yiisoft/definitions` is used only in `config/di.php`, which is loaded by consumers using
+    // `yiisoft/di`, that already requires `yiisoft/definitions` itself.
+    ->ignoreErrorsOnPackages(['yiisoft/definitions'], [ErrorType::SHADOW_DEPENDENCY]);
